95a1243df3bf75dc234b3781178bc1a2ccc26f42
Aaron Law <aaronlaw@gmail.com>
2020-09-27 23:05:55 +0800

    Add scripts in ./scripts-of-services.

---
+++ scripts-of-services/7zip批量压缩.bat
@@ -0,0 +1,3 @@
+REM Google: 7z 批量压缩 -> https://tophat.top/posts/200219c8.html
+
+for /d %%X in (*) do "D:\Program Files\7-Zip\7z.exe" a "%%X.7z" "%%X\"
---
+++ scripts-of-services/清除系統垃圾.bat
@@ -0,0 +1,28 @@
+REM @2009-01-03
+@echo off
+echo 正在清除系統垃圾檔案中，請稍候......
+del /f /s /q %systemdrive%\*.tmp
+del /f /s /q %systemdrive%\*._mp
+del /f /s /q %systemdrive%\*.log
+del /f /s /q %systemdrive%\*.gid
+del /f /s /q %systemdrive%\*.chk
+del /f /s /q %systemdrive%\*.old
+del /f /s /q %systemdrive%\recycled\*.*
+del /f /s /q %windir%\*.bak
+del /f /s /q %windir%\prefetch\*.*
+del /f /q %userprofile%\cookies\*.*
+del /f /q %userprofile%\recent\*.*
+del /f /s /q "%userprofile%\Local Settings\Temporary Internet Files\*.*"
+del /f /s /q "%userprofile%\Local Settings\Temp\*.*"
+del /f /s /q "%userprofile%\recent\*.*"
+DEL /S /F /Q "%systemroot%\Temp\*.*"
+DEL /S /F /Q "%AllUsersProfile%\「開始」功能表\程式集\Windows Messenger.lnk"
+RD /S /Q %windir%\temp & md %windir%\temp
+RD /S /Q "%userprofile%\Local Settings\Temp"
+MD "%userprofile%\Local Settings\Temp"
+RD /S /Q "%systemdrive%\Program Files\Temp"
+MD "%systemdrive%\Program Files\Temp"
+RD /S /Q "%systemdrive%\d"
+net user aspnet /delete
+echo 清除系統垃圾檔案完成！！
+echo. & pause
